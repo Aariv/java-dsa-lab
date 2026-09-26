df01600af84d9b7e3ef227fbd4329ee935ccf511
Aariv <arivbits@gmail.com>
2026-09-26 23:26:26 +0530

    Kth Largest Element, Cycle and reverse

--- problem-solving/src/main/java/com/ariv/problemsolving/patterns/heap/KthLargestElement.java
+++ problem-solving/src/main/java/com/ariv/problemsolving/patterns/heap/KthLargestElement.java
@@ -1,4 +1,27 @@
 package com.ariv.problemsolving.patterns.heap;
 
+import java.util.PriorityQueue;
+
 public class KthLargestElement {
+
+    public int findKthLargest(int[] nums, int k) {
+        // Create a min heap with the first k elements of the array
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (int i = 0; i < k; i++) {
+            minHeap.offer(nums[i]);
+        }
+
+        // Iterate through the rest of the array
+        for (int i = k; i < nums.length; i++) {
+            // If the current element is greater than the smallest element in the heap
+            if (nums[i] > minHeap.peek()) {
+                // Remove the smallest element and add the current element to the heap
+                minHeap.poll();
+                minHeap.offer(nums[i]);
+            }
+        }
+
+        // The root of the heap is the kth largest element
+        return minHeap.peek();
+    }
 }
--- problem-solving/src/main/java/com/ariv/problemsolving/patterns/linkedlist/ReverseLL.java
+++ problem-solving/src/main/java/com/ariv/problemsolving/patterns/linkedlist/ReverseLL.java
@@ -1,4 +1,26 @@
 package com.ariv.problemsolving.patterns.linkedlist;
 
 public class ReverseLL {
+
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        while (current != null) {
+            ListNode nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+        return prev;
+    }
+
+    private class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
 }
